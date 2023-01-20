926b592d460bb1148392c320546f9e8b64893a90
Anna Volfman <aniya_07@ukr.net>
2023-01-20 12:36:11 +0200

    Update apiMainPage.js

--- src/js/templates/apiMainPage.js
+++ src/js/templates/apiMainPage.js
@@ -4,7 +4,10 @@ import { FilmsApi } from '../api';
 
 const filmsApi = new FilmsApi();
 
-// const inputEl = document.querySelector('')
+const formEl = document.querySelector('.search-form');
+const buttonEl = document.querySelector('.search-form__button');
+
+formEl.addEventListener('submit', onSearchFormSubmit);
 
 filmsApi
     .fetchTrendingFilms()
@@ -16,40 +19,55 @@ filmsApi
     console.log(err);
     });
 
-function onSearchFormSubmit() {
-    // event.preventDefault();
-    // searchBtnEl.disabled = true;
-    filmsApi.query = 'dog';
-    // unsplashAPI.page = 1;
+function onSearchFormSubmit(event) {
+    event.preventDefault();
 
+    document.body.innerHTML = '';
+    buttonEl.disabled = true;
+
+    filmsApi.query = event.target.elements[0].value.trim();
+    filmsApi.page = 1;
+
+    if (!filmsApi.query) {
+        return
+    }
+    
     filmsApi
         .fetchFilmsByQuery()
         .then(({ data }) => {
-            console.log(data.results)
-            renderGalleryFilms(data.results)
+
+            if (!data.results.length) {
+                alert('nothing gets');
+                event.target.reset();
+                document.body.innerHTML = '';
+                return;
+            }
+
+            renderGalleryFilms(data.results);
         })
         .catch(err => {
             console.log(err);
-        });
+        })
+        .finally(buttonEl.disabled = false);
 }
 
 
-// function renderGalleryFilms(films) {
-//     const markup = films.map((film) => {
-//         return `
-//             <div>
-//                 <img src="https://image.tmdb.org/t/p/w500/${film.poster_path}" alt="${film.original_title}" width='395'  />
-//                 <div>
-//                     <b class='card-title'>${film.original_title}</b>
-//                     <div class='card-container'>
-//                     <p class='card-genre'>${film.genre_ids}</p><span class='card-year'>${getYear(film.release_date)}</span><span class='card-average'>${film.vote_average}</span>
-//                     </div>
-//                 </div>
-//             </div>
-//         `;
-//     }).join('');
-//     document.body.insertAdjacentHTML("beforeend", markup);
-// }
+function renderGalleryFilms(films) {
+    const markup = films.map((film) => {
+        return `
+            <div>
+                <img src="https://image.tmdb.org/t/p/w500/${film.poster_path}" alt="${film.original_title}" width='395'  />
+                <div>
+                    <b class='card-title'>${film.original_title}</b>
+                    <div class='card-container'>
+                    <p class='card-genre'>${film.genre_ids}</p><span class='card-year'>${getYear(film.release_date)}</span><span class='card-average'>${film.vote_average}</span>
+                    </div>
+                </div>
+            </div>
+        `;
+    }).join('');
+    document.body.insertAdjacentHTML("beforeend", markup);
+}
 
 function getYear(stringDate) {
     return stringDate.split('-')[0];
