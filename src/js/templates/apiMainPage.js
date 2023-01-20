ce70c22e0174ea7365b49abd8fce0588c56e7bc0
Anna Volfman <aniya_07@ukr.net>
2023-01-20 11:13:30 +0200

    Update apiMainPage.js

--- src/js/templates/apiMainPage.js
+++ src/js/templates/apiMainPage.js
@@ -34,22 +34,22 @@ function onSearchFormSubmit() {
 }
 
 
-function renderGalleryFilms(films) {
-    const markup = films.map((film) => {
-        return `
-            <div>
-                <img src="https://image.tmdb.org/t/p/w500/${film.poster_path}" alt="${film.original_title}" width='395'  />
-                <div>
-                    <b class='card-title'>${film.original_title}</b>
-                    <div class='card-container'>
-                    <p class='card-genre'>${film.genre_ids}</p><span class='card-year'>${getYear(film.release_date)}</span><span class='card-average'>${film.vote_average}</span>
-                    </div>
-                </div>
-            </div>
-        `;
-    }).join('');
-    document.body.insertAdjacentHTML("beforeend", markup);
-}
+// function renderGalleryFilms(films) {
+//     const markup = films.map((film) => {
+//         return `
+//             <div>
+//                 <img src="https://image.tmdb.org/t/p/w500/${film.poster_path}" alt="${film.original_title}" width='395'  />
+//                 <div>
+//                     <b class='card-title'>${film.original_title}</b>
+//                     <div class='card-container'>
+//                     <p class='card-genre'>${film.genre_ids}</p><span class='card-year'>${getYear(film.release_date)}</span><span class='card-average'>${film.vote_average}</span>
+//                     </div>
+//                 </div>
+//             </div>
+//         `;
+//     }).join('');
+//     document.body.insertAdjacentHTML("beforeend", markup);
+// }
 
 function getYear(stringDate) {
     return stringDate.split('-')[0];
