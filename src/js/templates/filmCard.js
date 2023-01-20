a829d23e1873d3f7536dd589412e1300b2b7c9ca
Olesia Romanienkova <110686374+Romanienkova@users.noreply.github.com>
2023-01-20 13:13:50 +0200

    Update filmCard.js

--- src/js/templates/filmCard.js
+++ src/js/templates/filmCard.js
@@ -1,16 +1,18 @@
-export const createFilmCards = cardInfo => {
-	const filmCardsArr = cardInfo.map(
-    el =>
+'use strict';
+
+export default const createFilmCards = films => {
+	const filmsMarkup = films.map(
+    film =>
       `
 			<li>
-				<img src="${el.poster_path}" alt="${el.title}" width='395' height='574'>
-   			<div  class="film__description">
-   				<h2>${el.title}</h2>
-	   			<p class="film__description--more">${el.genres} | ${el.release_date}</p>
+				<img src="https://image.tmdb.org/t/p/w500/${film.poster_path}" alt="${film.original_title}" width='395'>
+   			<div class="film__description">
+   				<h2>${film.original_title}</h2>
+	   			<p class="film__description--more">${film.genre_ids} | ${getYear(film.release_date)} | ${film.vote_average}</p>
    			</div>
 			</li>
       `
-  );
+  ).join('');
 
-  	return filmCardsArr.join('');
+    document.body.insertAdjacentHTML("beforeend", filmsMarkup);
 };
