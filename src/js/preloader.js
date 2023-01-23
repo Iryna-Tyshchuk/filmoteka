4a5fba62d91d1fd42ed5eef31d4b400d07feac95
Olesia Romanienkova <110686374+Romanienkova@users.noreply.github.com>
2023-01-23 23:48:09 +0200

    loader

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,16 +1,35 @@
-window.addEventListener('load', () => {
-	let preloader = document.querySelector('.preloader');
-	preloader.classList.add('disappear');
-});
-// let preloaderRef = document.getElementById('preloader');
+document.getElementById('preloader');
 
-// window.addEventListener('load', closePreloader);
-// function closePreloader() {
-//   setTimeout(() => {
-//     document.body.classList.add('disappear');
-//     setTimeout(() => {
-//       document.body.classList.add('preloader');
-//       document.body.classList.remove('disappear');
-//     }, 1000);
-//   }, 1000);
-// }
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.body.classList.add('disappear');
+    setTimeout(() => {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('disappear');
+    }, 500);
+  }, 500);
+});
+
+let toggler = document.querySelector('.toolbar-container');
+
+toggler.addEventListener('input', () => {
+  setTimeout(() => {
+    document.body.classList.add('disappear');
+    setTimeout(() => {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('disappear');
+    }, 500);
+  }, 500);
+});
+
+let onClickfilmCard = document.querySelector('.film__list-element');
+
+onClickfilmCard.addEventListener('click', () => {
+  setTimeout(() => {
+    document.body.classList.add('disappear');
+    setTimeout(() => {
+      document.body.classList.add('loaded');
+      document.body.classList.remove('disappear');
+    }, 500);
+  }, 500);
+});
