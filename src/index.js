26b22390e3fd5ea142578a7b4957800e96d5ba19
katefranchuk <franchukekaterina999@gmail.com>
2023-01-24 00:01:43 +0200

    to top btn

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import localStorageService from './js/localstorage.js';
 
 import comingSoon from './js/templates/comingSoon.js';
 
+// import showScrollup from './js/showScrollup';
+
 import apiMainPage from './js/templates/apiMainPage';
 import './js/templates/modal-film-card';
