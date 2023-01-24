7641355b0b17a0e4bb2b828c0fd14c1f8af6c2be
YukhymykAlena <o.yukhymyk@gmail.com>
2023-01-24 11:41:20 +0200

    modal-footer

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import './js/templates/modal-film-card';
 import queuList from './js/templates/queueList';
 import showScrollup from './js/showScrollup';
 import './js/preloader';
+import './js/templates/modalCreators';
