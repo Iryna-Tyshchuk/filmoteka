76ad182ddb1cb6ab7d780928189f7e66f2407420
katefranchuk <franchukekaterina999@gmail.com>
2023-01-24 21:51:53 +0200

    form

--- src/js/showScrollup.js
+++ src/js/showScrollup.js
@@ -1,5 +1,6 @@
-function scrollUp()  {
+function scrollUp () {
   const scrollUp = document.querySelector('#scroll-up');
+ 
   // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
   this.scrollY >= 300
     ? scrollUp.classList.add('show-scroll')
